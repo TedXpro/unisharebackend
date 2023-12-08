4351e4c9da87ca604a06cb7d286713d94343dc03
TedXpro <johannes.woldeyes@gmail.com>
2023-12-08 04:47:07 +0300

    inital commit

---
+++ src/main/java/com/unishare/unisharebackend/UnisharebackendApplication.java
@@ -0,0 +1,13 @@
+package com.unishare.unisharebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnisharebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnisharebackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unishare/unisharebackend/UnisharebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unishare.unisharebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnisharebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
